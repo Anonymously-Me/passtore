1530a08c01e9fb796c0f6c2dcb3863a1d92b7ba2
Anonymously-Me <39085053+Anonymously-Me@users.noreply.github.com>
2020-10-14 17:51:23 +0300

    Update passtoreCLI.py
    Updated the program to Python3, no longer works Python2

--- passtoreCLI.py
+++ passtoreCLI.py
@@ -7,10 +7,10 @@ from hashlib import sha256, md5
 from pickle import loads, dumps
 import shutil
 import gzip
-import anydbm
+import dbm
 
 system("title PASSTORE")
-dat = anydbm.open("main.db", "c")
+dat = dbm.open("main.db", "c")
 system("attrib +s +h main.db")
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
             'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
@@ -77,27 +77,27 @@ if len(dat) == 0:
         key1 = getpass("Enter your encryption key\n: ")
         if len(master) < 8 or len(key1) < 8:
             system("cls")
-            print "BOTH MASTER PASSWORD AND ENCRYPTION KEY SHOULD HAVE A MINIMUM OF 8 CHARACTERS\n"
+            print("BOTH MASTER PASSWORD AND ENCRYPTION KEY SHOULD HAVE A MINIMUM OF 8 CHARACTERS\n")
             system("pause")
             continue
         key4 = getpass("Enter the same encryption key again\n: ")
         if key4 == key1:
             system("cls")
             if master == key4:
-                print "ENTER A DIFFERENT MASTER PASSWORD AND ENCRYPTION KEY\n\n\n"
+                print("ENTER A DIFFERENT MASTER PASSWORD AND ENCRYPTION KEY\n\n\n")
                 system("pause")
                 continue
             dat["master"] = dumps(encrypted(key4, master))
             dat.close()
-            dat = anydbm.open("main.db", "c")
+            dat = dbm.open("main.db", "c")
             forget = password()
             dat["forget"] = dumps(encrypted(key4, forget))
-            print "Your forget code(code used when you forget your password) is\n: ", forget, "\n\n\n"
-            print "IT IS USED TO RECOVER A FORGOTTEN PASSWORD\n"
+            print("Your forget code(code used when you forget your password) is\n: ", forget, "\n\n\n")
+            print("IT IS USED TO RECOVER A FORGOTTEN PASSWORD\n")
             system("pause")
             break
         else:
-            print "\n\nTHE KEYS YOU ENTERED ARE NOT THE SAME!!!\n\n"
+            print("\n\nTHE KEYS YOU ENTERED ARE NOT THE SAME!!!\n\n")
             system("pause")
 
 
@@ -105,7 +105,7 @@ while True:
     system("cls")
     mannered = loads(dat["master"])
     forgotten = loads(dat["forget"])
-    master = raw_input("Enter your master password, or forget code if you forgot your password\n: ")
+    master = input("Enter your master password, or forget code if you forgot your password\n: ")
     if master != "":
         keg = getpass("Enter your encryption key\n: ")
         mastered = decrypted(keg, master, "master")
@@ -117,33 +117,33 @@ while True:
                 key1 = getpass("Enter your new encryption key\n: ")
                 if len(master) < 8 or len(key1) < 8:
                     system("cls")
-                    print "BOTH MASTER PASSWORD AND ENCRYPTION KEY SHOULD HAVE A MINIMUM OF 8 CHARACTERS\n"
+                    print("BOTH MASTER PASSWORD AND ENCRYPTION KEY SHOULD HAVE A MINIMUM OF 8 CHARACTERS\n")
                     system("pause")
                     continue
                 key4 = getpass("Enter the same new encryption key again\n: ")
                 if key4 == key1:
                     system("cls")
                     if master == key4:
-                        print "ENTER A DIFFERENT MASTER PASSWORD AND ENCRYPTION KEY\n\n\n"
+                        print("ENTER A DIFFERENT MASTER PASSWORD AND ENCRYPTION KEY\n\n\n")
                         system("pause")
                         continue
                     dat["master"] = str(encrypted(key4, master))
                     dat.close()
-                    dat = anydbm.open("main.db", "c")
+                    dat = dbm.open("main.db", "c")
                     forget = password()
                     dat["forget"] = str(encrypted(key4, forget))
-                    print "Your forget code(code used when you forget your password) is\n:", forget, "\n\n\n"
-                    print "IT IS USED TO RECOVER A FORGOTTEN PASSWORD\n"
+                    print("Your forget code(code used when you forget your password) is\n:", forget, "\n\n\n")
+                    print("IT IS USED TO RECOVER A FORGOTTEN PASSWORD\n")
                     system("pause")
                     break
                 else:
-                    print "\n\nTHE KEYS YOU ENTERED ARE NOT THE SAME!!!\n\n"
+                    print("\n\nTHE KEYS YOU ENTERED ARE NOT THE SAME!!!\n\n")
                     system("pause")
         elif mastered == mannered[0]:
             while True:
-                dat = anydbm.open("main.db", "c")
+                dat = dbm.open("main.db", "c")
                 system("cls")
-                inp = raw_input("Menu:\n1) Create a new password\n2) "
+                inp = input("Menu:\n1) Create a new password\n2) "
                                 "View your passwords\n3) Remove a password\n"
                                 "4) Change a password\n"
                                 "5) Backup\n6) About\n7) Exit\n: ")
@@ -153,8 +153,8 @@ while True:
                     mastered = decrypted(keg, master, "master")
                     if mastered == mannered[0]:
                         system("cls")
-                        print "Enter the name of the account you want to set a password for"
-                        name = raw_input("or press enter to go back to the main menu\n: ")
+                        print("Enter the name of the account you want to set a password for")
+                        name = input("or press enter to go back to the main menu\n: ")
                         if name != "":
                             system("cls")
                             get = input("Enter:\n1) Generate a password\n2) Enter a custom password\n: ")
@@ -162,14 +162,14 @@ while True:
                             if get == 1:
                                 dat[name] = encrypted(keg, password())
                                 dat.close()
-                                print "\nYOUR NEW PASSWORD HAS BEEN ADDED!!\n\n"
+                                print("\nYOUR NEW PASSWORD HAS BEEN ADDED!!\n\n")
                                 system("pause")
                             elif get == 2:
                                 system("cls")
                                 news = getpass("Enter your password\n: ")
                                 dat[name] = encrypted(keg, news)
                                 dat.close()
-                                print "\n\nYOUR PASSWORD HAS BEEN ADDED!!!\n\n"
+                                print("\n\nYOUR PASSWORD HAS BEEN ADDED!!!\n\n")
                                 system("pause")
                 elif inp == "2":
                     system("cls")
@@ -178,7 +178,7 @@ while True:
                     if mastered == mannered[0]:
                         system("cls")
                         system("cls")
-                        gen = raw_input("Enter:\n1) To view all passwords\n2)"
+                        gen = input("Enter:\n1) To view all passwords\n2)"
                                         " To view a specific password\n3) Back\n: ")
                         if gen == "1":
                             system("cls")
@@ -189,12 +189,12 @@ while True:
                                 if len(dat) > 1:
                                     for i in dat:
                                         if i != "master" and i != "forget":
-                                            print i + ": " + decrypts(keg, dat[i])
+                                            print(i + ": " + decrypts(keg, dat[i]))
                                         else:
                                             continue
                                 else:
-                                    print "NO SAVED PASSWORDS!!!"
-                                print "\n\n"
+                                    print("NO SAVED PASSWORDS!!!")
+                                print("\n\n")
                                 system("pause")
                         elif gen == "2":
                             system("cls")
@@ -203,17 +203,17 @@ while True:
                             if mastered == mannered[0]:
                                 system("cls")
                                 if len(dat) == 1:
-                                    print "NO SAVED PASSWORDS!!!"
+                                    print("NO SAVED PASSWORDS!!!")
                                 else:
-                                    che = raw_input("Enter the name of the account\n: ")
+                                    che = input("Enter the name of the account\n: ")
                                     che = che.upper()
                                     system("cls")
                                     if che not in dat:
-                                        print "NO ACCOUNT WITH THIS NAME!!!\n\n"
+                                        print("NO ACCOUNT WITH THIS NAME!!!\n\n")
                                         system("pause")
                                         if che in dat and che != "master" and che != "forget":
-                                            print che + ": " + decrypts(keg, dat[che])
-                                print "\n\n"
+                                            print(che + ": " + decrypts(keg, dat[che]))
+                                print("\n\n")
                                 dat.close()
                                 system("pause")
                 elif inp == "3":
@@ -222,11 +222,11 @@ while True:
                     mastered = decrypted(keg, master, "master")
                     if mastered == mannered[0]:
                         system("cls")
-                        iexp = raw_input("Enter the account you would like to remove\n: ")
+                        iexp = input("Enter the account you would like to remove\n: ")
                         iexp = iexp.upper()
                         if iexp in dat:
                             del dat[iexp]
-                            print "\n\nTHE ACCOUNT HAS BEEN DELETED\n\n"
+                            print("\n\nTHE ACCOUNT HAS BEEN DELETED\n\n")
                             system("pause")
                             break
                         dat.close()
@@ -236,20 +236,20 @@ while True:
                     mastered = decrypted(keg, master, "master")
                     if mastered == mannered[0]:
                         system("cls")
-                        iexp = raw_input("Enter the account you would like to change the password\n: ")
+                        iexp = input("Enter the account you would like to change the password\n: ")
                         iexp = iexp.upper()
                         if iexp in dat:
                             old = getpass("Enter the old password\n: ")
                             if old == decrypts(keg, dat[iexp]):
                                 dat[iexp] = encrypted(keg, password())
                                 dat.close()
-                                print "\n\nTHE PASSWORD HAS BEEN CHANGED!!!\n\n"
+                                print("\n\nTHE PASSWORD HAS BEEN CHANGED!!!\n\n")
                                 system("pause")
                 elif inp == "5":
                     system("cls")
                     enter = input("1) Backup your passwords\n2) Open a backup file\n: ")
                     if enter == 1:
-                        direct = raw_input("Enter the directory to save the backup to\n: ")
+                        direct = input("Enter the directory to save the backup to\n: ")
                         back = open("main.db", "rb")
                         backup = gzip.open("BACKUP", "wb")
                         shutil.copyfileobj(back, backup)
@@ -257,7 +257,7 @@ while True:
                         backup.close()
                         shutil.move("BACKUP", direct)
                     elif enter == 2:
-                        direct_in = raw_input("Enter the full directory of the backup file\n: ")
+                        direct_in = input("Enter the full directory of the backup file\n: ")
                         shutil.copy(direct_in, getcwd())
                         back_in = gzip.open("BACKUP", "rb")
                         back_out = open("main1.db", "wb")
@@ -271,14 +271,14 @@ while True:
                         system("attrib +s +h main.db")
                 elif inp == "6":
                     system("cls")
-                    print "PASSSTORE version 1.3.0"
+                    print("PASSSTORE version 1.3.0")
                     sleep(1)
-                    print "\nPROGRAMMED BY:\n-------------\nKAMIL MUKTAR\n\n"
-                    print "SPECIAL THANKS TO:\n-------\"------\"--"
-                    print "NADIA AHMED\nMUKTAR ABDURAHIM\nFAKIHA ABDURAHIM\nDR. ALIA IBRAHIM\n\n"
+                    print("\nPROGRAMMED BY:\n-------------\nKAMIL MUKTAR\n\n")
+                    print("SPECIAL THANKS TO:\n-------\"------\"--")
+                    print("NADIA AHMED\nMUKTAR ABDURAHIM\nFAKIHA ABDURAHIM\nDR. ALIA IBRAHIM\n\n")
                     sleep(3)
-                    print "THANKS TO:\n------\"--"
-                    print "ELIAS AMHA\nROBEL GRMAY\nABINET TASSEW\n\n"
+                    print("THANKS TO:\n------\"--")
+                    print("ELIAS AMHA\nROBEL GRMAY\nABINET TASSEW\n\n")
                     sleep(1)
                     system("pause")
                 elif inp == "7":
@@ -286,5 +286,5 @@ while True:
                     exit(1)
                 else:
                     system("cls")
-                    print "\n\n ENTER A VALID CHOICE!!!"
-            dat = anydbm.open("main.db", "c")
+                    print("\n\n ENTER A VALID CHOICE!!!")
+            dat = dbm.open("main.db", "c")
